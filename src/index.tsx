41fd10a2cd4d0ab9f25b0e29843e0144c26fcc23
Shane Kennedy <shane.kennedy19@gmail.com>
2024-03-28 11:50:05 +0100

    list view to start, no forms

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,33 @@
 import React, { useEffect, useState } from "react";
 import yahooFinance from "yahoo-finance2";
 import { Quote } from "yahoo-finance2/dist/esm/src/modules/quote";
+import { tickers } from "./tickers";
 
-import { ActionPanel, Form, Action, Detail, useNavigation } from "@raycast/api";
+import { ActionPanel, Form, Action, Detail, useNavigation, List } from "@raycast/api";
 
 export default function Command() {
+  return (
+    <List navigationTitle="Search for quotes" searchBarPlaceholder="Search for quotes">
+      {tickers.map((t) => (
+        <List.Item
+          key={t.symbol}
+          title={`${t.symbol} ${t.name}`}
+          actions={
+            <ActionPanel title="#1 in raycast/extensions">
+              <Action.Push title="Get Quote" target={<QuoteView symbol={t.symbol} />} />
+              <Action.OpenInBrowser
+                title="See on finance.yahoo.com"
+                url={`https://finance.yahoo.com/quote/${t.symbol}`}
+              />
+            </ActionPanel>
+          }
+        />
+      ))}
+    </List>
+  );
+}
+
+export function QuoteForm() {
   const { push } = useNavigation();
   const [symbol, setSymbol] = useState("NVDA");
 
